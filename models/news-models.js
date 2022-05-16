93340ab670f92110faf756649f5bea982b55aa7f
Enamul Ali <enamulali@Enamuls-MacBook-Pro.local>
2022-05-16 19:48:49 +0100

    add selectArticleByID function

--- models/news-models.js
+++ models/news-models.js
@@ -1,9 +1,24 @@
 const db = require("../db/connection");
 
 exports.selectTopics = () => {
-  console.log("Hello");
   const queryStr = "SELECT * FROM topics";
   return db.query(queryStr).then((topics) => {
     return topics.rows;
   });
+};
+
+exports.selectArticleByID = (articleid) => {
+  return db
+    .query("SELECT * FROM articles WHERE article_id = $1", [articleid])
+    .then((result) => {
+      const myArticle = result.rows;
+      if (myArticle.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: `article with id: ${articleid} does not exist`,
+          detail: `please enter valid article number`,
+        });
+      }
+      return myArticle[0];
+    });
 };
