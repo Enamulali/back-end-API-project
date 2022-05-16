791ba19e7d755e79d5265faa5387ff8c4d64f719
Enamul Ali <enamulali@Enamuls-MacBook-Pro.local>
2022-05-16 13:09:32 +0100

    create first get request

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { getTopics } = require("./controllers/news-controllers");
+
+const app = express();
+
+app.get("/api/topics", getTopics);
+
+//custom errors
+app.all("/*", (req, res, next) => {
+  next({ status: 404, msg: "not found", detail: "inavlid endpoint/method" });
+});
+
+app.use((err, req, res, next) => {
+  if (err.status) {
+    res.status(err.status).send({ msg: err.msg, detail: err.detail });
+  }
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ msg: "internal server error" });
+});
+
+module.exports = app;
