21b3b2975540c2113d932a8fb55ac186b7746311
Enamul Ali <enamulali@enamuls-mbp.home>
2022-05-17 12:05:10 +0100

    added patch articles vote count

--- models/articles.model.js
+++ models/articles.model.js
@@ -4,7 +4,7 @@ exports.selectArticleByID = (articleid) => {
   return db
     .query("SELECT * FROM articles WHERE article_id = $1", [articleid])
     .then((result) => {
-      if (result.rows.length === 0) {
+      if (!result.rows.length) {
         return Promise.reject({
           status: 404,
           msg: `article with id: ${articleid} does not exist`,
@@ -13,4 +13,20 @@ exports.selectArticleByID = (articleid) => {
       }
       return result.rows[0];
     });
+};
+
+exports.updateArticleByID = (inc_votes, articleid) => {
+  const queryStr = `UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *`;
+  const values = [inc_votes, articleid];
+  return db.query(queryStr, values).then((result) => {
+    if (!result.rows.length) {
+      return Promise.reject({
+        status: 404,
+        msg: `article with id: ${articleid} does not exist`,
+        detail: `please enter valid article number`,
+      });
+    }
+
+    return result.rows[0];
+  });
 };
