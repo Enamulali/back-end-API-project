be7b3d1ebeb4c56b3070ff9a921a0833318326fe
Enamul Ali <enamulali@enamuls-mbp.home>
2022-05-17 12:04:50 +0100

    added patch article votes

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,8 +1,23 @@
-const { selectArticleByID } = require("../models/articles.model");
+const {
+  selectArticleByID,
+  updateArticleByID,
+} = require("../models/articles.model");
 
 exports.getArticleByID = (req, res, next) => {
   const { articleid } = req.params;
   selectArticleByID(articleid)
+    .then((result) => {
+      res.status(200).send({ article: result });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.patchArticleByID = (req, res, next) => {
+  const { articleid } = req.params;
+  const { inc_votes } = req.body;
+  updateArticleByID(inc_votes, articleid)
     .then((result) => {
       res.status(200).send({ article: result });
     })
