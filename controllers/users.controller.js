214dbbd5eaa805a762abe72e1e84ef6974a451e7
Enamul Ali <enamulali@enamuls-mbp.home>
2022-05-17 14:27:21 +0100

    change res.status to explicit return

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -2,7 +2,9 @@ const { selectUsers } = require("../models/users.model");
 
 exports.getUsers = (req, res, next) => {
   selectUsers()
-    .then((users) => res.status(200).send({ users }))
+    .then((users) => {
+      res.status(200).send({ users });
+    })
     .catch((err) => {
       next(err);
     });
