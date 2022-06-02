e9f880ecdaee03bb0c300ca82aa9ef681a2868fc
Enamul Ali <enamulali@enamuls-mbp.home>
2022-06-02 15:47:47 +0100

    heroku

--- listen.js
+++ listen.js
@@ -1,7 +1,5 @@
 const app = require("./app");
 
-const PORT = 8080;
+const { PORT = 9090 } = process.env;
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
